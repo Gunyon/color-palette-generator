43495aa4f804e02d2ac214a5ccaf1591e3948a66
Oleg Gabureac <gabureac.oleg@gmail.com>
2023-03-11 13:56:07 +0200

    add 2 more palette

--- src/App.svelte
+++ src/App.svelte
@@ -7,12 +7,53 @@
 
   let materialPalette = [];
   let chromaPalette = [];
+  let shadesPalette = [];
+  let swatchesPalette = [];
 
   const handleChangeColor = event => {
     const hexColor = event.target.value;
     materialPalette = generateMaterialPalette(hexColor);
     chromaPalette = generateChromaJsPalette(hexColor);
+    shadesPalette = generatePaletteFromShades(hexColor);
+    swatchesPalette = getSwatches(hexColor);
   };
+
+  function getSwatches(hexColor) {
+    let primaryColor = chroma(hexColor);
+    // Define the range of saturation and lightness values for the color swatches
+    const lightnessRange = [0.12, 0.96];
+    const numSwatches = 10;
+
+    // Generate the swatches
+    const swatches = [];
+    for (let i = 0; i < numSwatches; i++) {
+      const saturation = (primaryColor.get('hsl.s') * (i + 1)) / numSwatches;
+      const lightness =
+        lightnessRange[0] +
+        ((lightnessRange[1] - lightnessRange[0]) * i) / (numSwatches - 1);
+      const swatch = primaryColor
+        .set('hsl.s', saturation)
+        .set('hsl.l', lightness);
+      swatches.push(swatch);
+    }
+
+    // Apply contrast rules
+    for (let i = 1; i < swatches.length; i++) {
+      const prevSwatch = swatches[i - 1];
+      const currSwatch = swatches[i];
+      const contrastRatio = chroma.contrast(prevSwatch, currSwatch);
+      if (contrastRatio < 4.5) {
+        const lightnessDiff =
+          (4.5 * prevSwatch.get('hsl.l') - currSwatch.get('hsl.l') * 3.5) / 1.5;
+        const newLightness = Math.min(
+          Math.max(lightnessDiff, lightnessRange[0]),
+          lightnessRange[1]
+        );
+        swatches[i] = currSwatch.set('hsl.l', newLightness);
+      }
+    }
+    return addContrast(swatches.map(s => s.hex()));
+  }
 
   function generateMaterialPalette(hex) {
     const argb = argbFromHex(hex);
@@ -23,11 +64,6 @@
       const hex = hexFromArgb(current);
       colors.push(hex);
     });
-    // for (let i = 100; i >= 30; i = i - 7) {
-    //   const current = TonalPalette.fromInt(argb).tone(i);
-    //   const hex = hexFromArgb(current);
-    //   colors.push(hex);
-    // }
     return addContrast(colors);
   }
 
@@ -38,6 +74,21 @@
       .colors(10);
 
     return addContrast(colorPalette.reverse());
+  }
+
+  function generatePaletteFromShades(baseColor) {
+    const colors = [];
+
+    // Define the shades to generate
+    const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+    shades.forEach(shade => {
+      const value = chroma(baseColor)
+        .darken(shade / 100)
+        .hex();
+      colors.push(value);
+    });
+    return addContrast(colors);
   }
 
   function addContrast(colors) {
@@ -69,9 +120,34 @@
         </div>
       {/each}
     </div>
+
     <div class="col">
       <h2>Chroma palette</h2>
       {#each chromaPalette as color}
+        <div
+          class="color-item"
+          style="background-color: {color.color}; color: {color.contrast}"
+        >
+          {color.color}
+        </div>
+      {/each}
+    </div>
+
+    <div class="col">
+      <h2>Shades palette</h2>
+      {#each shadesPalette as color}
+        <div
+          class="color-item"
+          style="background-color: {color.color}; color: {color.contrast}"
+        >
+          {color.color}
+        </div>
+      {/each}
+    </div>
+
+    <div class="col">
+      <h2>Swatches palette</h2>
+      {#each swatchesPalette as color}
         <div
           class="color-item"
           style="background-color: {color.color}; color: {color.contrast}"
